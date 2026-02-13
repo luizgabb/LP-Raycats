624f53b9b84ab3b968803908d309432247345cf2
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2026-02-13 15:53:48 -0300

    adicionando angular cdr e iniciando lógica do menu da navbar para versão mobile

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { map, Observable, share, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  private BreakpointObserver = inject(BreakpointObserver)
+
+  isMobile$ : Observable<boolean> = this.BreakpointObserver
+  .observe('max-width: 950px')
+  .pipe(
+    map(result =>result.matches),
+    shareReplay()
+  )
+
+  isMenuOpen = false
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
 }
