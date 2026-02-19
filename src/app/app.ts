54c732004d420af34b1c2eee946f1eba522967bf
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2026-02-19 16:52:08 -0300

    criação do component projects e simulação do scroll com lista

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
 import { Main } from "./components/main/main";
+import { Projects } from "./components/projects/projects";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Main],
+  imports: [RouterOutlet, Navbar, Main, Projects],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
